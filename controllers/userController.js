a4e39f85e20612b14ccf63233322dac7e69f3c45
Ajilal Vijayan <ajilalv@gmail.com>
2021-10-20 15:50:42 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,24 @@
 /* ------> User Controller */
 
+const passport = require("../routes/passport.js");
+
 exports.mypage = function(req, res, next) {
   res.render("mypage", { user: req.user });
 };
 
 exports.userlogin = function(req, res, next) {
-  if (req.user) res.redirect("mypage");
-  else res.render("login");
+    passport.authenticate('azuread-openidconnect', 
+      { 
+        response: res,                      // required
+        //resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
+        customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
+        failureRedirect: '/' 
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    console.log('Login was called in the Sample');
+    res.redirect('/');
 };
 
 exports.userlogout = function(req, res, next) {
