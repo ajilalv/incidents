2f1e3efd7b58feef617a8e8e7f91e9ea5598ea08
Ajilal Vijayan <ajilalv@gmail.com>
2021-10-21 11:08:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -36,4 +36,5 @@ app.use("/user", userRouter);
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
+  console.log('TENT IS: ',process.ENV.TENANT_NAME)
 });
